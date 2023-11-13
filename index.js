47c44ccf3efc17d7cc3ada9a495e3c914164dd13
Veronica-Void <vvoid8075@gmail.com>
2023-11-13 10:22:28 -0800

    made space for merge

--- index.js
+++ index.js
@@ -1,28 +1,61 @@
-function onclickCells()
-{
-    var playerSym = "x"
-    var gEnded = false
-    if(playerSym = "x"){
-        playerSym = "o"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var winPosition = [[document.getElementById('1'),document.getElementById('2'),document.getElementById('3')],[document.getElementById('4'),document.getElementById('5'),document.getElementById('6')],[document.getElementById('7'),document.getElementById('8'),document.getElementById('9')], //horizontal 
+[document.getElementById('1'),document.getElementById ('4'),document.getElementById ('7')],[document.getElementById ('2'),document.getElementById ('5'),document.getElementById ('8')],[document.getElementById ('3'),document.getElementById ('6'),document.getElementById ('9')], //vertical 
+[document.getElementById ('1'),document.getElementById ('5'),document.getElementById ('9')],[document.getElementById ('3'),document.getElementById ('5'),document.getElementById ('7')]] //diagonal
+
+var winContainer = [winPosition.length]
+function winCheck(){
+    if (playerX() == [0,1,2]){
+        alert("Player X wins!")
     }
-    else{
-        playerSym = "x"
-    }
-    let tdcells = document.getElementsByClassName("cell")
-    for(i = 0; i < tdcells.length; i++){
-    tdcells.replace("", playerSym)
-    }
-    return playerSym;
 }
 
 
 
 
-// function winner(){
-//     var win = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-//     if(){}
 
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // function playerTurn()
