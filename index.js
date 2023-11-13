8598f491662d9c6a0973115a0b7d419c1f12cddd
sasso-gonzalez <assogonzalezs@gmail.com>
2023-11-13 13:02:17 -0500

    Finish clicking on cells and switching player's turn

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
-function onclickCells()
+var playerSym = "x"
+
+function onclickCells(cell)
 {
-    var playerSym = "x"
-    var gEnded = false
-    if(playerSym = "x"){
+    if(playerSym == "x")
+    {
         playerSym = "o"
     }
-    else{
+    else
+    {
         playerSym = "x"
     }
-    let tdcells = document.getElementsByClassName("cell")
-    for(i = 0; i < tdcells.length; i++){
-    tdcells.replace("", playerSym)
-    }
-    return playerSym;
+    cell.innerHTML = playerSym
+    cell.style.pointerEvents = "none"
+    let x = document.getElementById("player-turn").innerHTML = `Player's Turn: ${playerSym}`
 }
 
 
 
-// function winner(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function check_win(){
 //     var win = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-//     if(){}
 
 // }
 
-
-// function playerTurn()
-// {
-//     var x = document.getElementById("player-turn").getElementsByTagName("h4")
-//     for(var i = 0; i < x.length; i++){
-//         var rep = x[i].innerHTML.replace('.', switchTurn())
-//     }
-// }
 
 // var c = confirm(`_ Wins!`)
 // if()
